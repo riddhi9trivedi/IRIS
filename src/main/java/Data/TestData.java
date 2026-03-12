957f99e6ee08a611bb682c150a8ca2094afd163d
riddhi9trivedi <rtrivedi@corp.gdglobal.ca>
2026-03-12 13:08:18 -0400

    Deploy updated Allure report

---
+++ src/main/java/Data/TestData.java
@@ -0,0 +1,35 @@
+package Data;
+
+public class TestData {
+    public static final String APP_ID = "com.manh.storeui_0ke3e0bm2cnf2!com.manh.storeui";
+
+    public static final String USERNAME = "strmgr_can";
+    public static final String PASSWORD = "Welcome21!";
+
+    public static final String ASSOCIATE = "100443";
+    public static final String EMAIL_OPTION = "Associate:";
+
+    public static final String POS_INPUT1 = "0123456";
+    public static final String POS_INPUT2= "0587227";
+    public static final String POS_INPUT3 = "0587229";
+    public static final String POS_INPUT4 = "0659382";
+    public static final String ITEM_PRICE = "$15.00";
+    
+    public static final String Taxbeforecheckout = "Calculated at checkout";
+    public static final String Taxvalue = "$1.95";
+
+    
+    public static final String AMOUNT = "6000";
+    public static final String EMAIL_ADDRESS = "rtrivedi@dynamite.ca";
+
+    public static final String AUTH_BASE_URL = "https://gdyns2.omni.manh.com";
+    public static final String AUTH_ENDPOINT = "/oauth/token";
+    public static String ORDER_NUMBER;
+    public static String RORDER_NUMBER;
+    public static String EEORDER_NUMBER;
+    public static String PEORDER_NUMBER;
+    public static String NEORDER_NUMBER;
+
+    
+
+}
